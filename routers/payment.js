53cc59b05bdc84f98a4aeac503675748aea00a71
ritikjain6521 <168226242+ritikjain6521@users.noreply.github.com>
2025-07-25 14:19:45 +0530

    Update payment.js

--- routers/payment.js
+++ routers/payment.js
@@ -1,6 +1,6 @@
 import express from 'express'
 const  router =express.Router();
-import { checkout,verify,userOrder,allOrders} from '../controllers/Payment.js';
+import { checkout,verify,userOrder,allOrders} from '../controllers/payment.js';
 import { Authenticated } from '../Middlewares/auth.js';
 
 //checkout
