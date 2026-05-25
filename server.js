dfbd993ab003a1211bcd93db5d1bc39e91675db4
Ritik Jain <168226242+ritikjain6521@users.noreply.github.com>
2026-05-25 02:07:53 -0700

    fix the cors issues

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(bodyParser.json())
 app.use(cors({
-  origin:true,
+  origin:"https://ritikjain6521-find-a-repository-eco.vercel.app",
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
  credentials:true
 
